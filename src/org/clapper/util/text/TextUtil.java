5ebd44a95f58659a54c76ddab2cf81da7f7153ef
Brian Clapper <bmc@clapper.org>
2004-02-11 16:13:35 +0000

    Added.

---
+++ src/org/clapper/util/text/TextUtil.java
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------*\
+  $Id$
+\*---------------------------------------------------------------------------*/
+
+package org.clapper.util.text;
+
+/**
+ * Static class containing miscellaneous text utility methods.
+ *
+ * @version <tt>$Revision$</tt>
+ */
+public class TextUtils
+{
+    /*----------------------------------------------------------------------*\
+                                Constructor
+    \*----------------------------------------------------------------------*/
+
+    private TextUtils()
+    {
+    }
+
+    /*----------------------------------------------------------------------*\
+                              Public Methods
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * Convert a boolean string to a boolean value. This method is more
+     * generous than <tt>java.lang.Boolean.valueOf()</tt>. The following
+     * strings (in upper or lower case) are recognized as <tt>true</tt>:
+     * "1", "true", "yes", "y". The following
+     * strings (in upper or lower case) are recognized as <tt>false</tt>:
+     * "0", "false", "no", "n".
+     *
+     * @param s   string to convert
+     *
+     * @return <tt>true</tt> or <tt>false</tt>
+     *
+     * @throws IllegalArgumentException  string isn't a boolean
+     */
+    public static boolean booleanToString (String s)
+        throws IllegalArgumentException
+    {
+        boolean result;
+
+        s = s.toLowerCase();
+
+        if ((s.equals ("true") ||
+             s.equals ("1")    ||
+             s.equals ("yes")  ||
+             s.equals ("y")))
+        {
+            result = true;
+        }
+
+        else if ((s.equals ("false") ||
+                  s.equals ("0")     ||
+                  s.equals ("no")    ||
+                  s.equals ("n")))
+        {
+            result = false;
+        }
+
+        else
+        {
+            throw new IllegalArgumentException ("Bad boolean string: \""
+                                              + s
+                                              + "\"");
+        }
+
+        return result;
+    }
+}
