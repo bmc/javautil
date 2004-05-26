a75b1d5e10551286a1cebf28c5813f66f362b8f9
Brian Clapper <bmc@clapper.org>
2004-05-26 18:50:53 +0000

    Added

---
+++ src/org/clapper/util/text/Unicode.java
@@ -0,0 +1,64 @@
+/*---------------------------------------------------------------------------*\
+  $Id$
+\*---------------------------------------------------------------------------*/
+
+package org.clapper.util.text;
+
+/**
+ * Symbolic names for certain Unicode constants. The constant
+ * names are adapted from the Unicode standard. See
+ * <a href="http://charts.unicode.org/"><i>charts.unicode.org</i></a>.
+ *
+ * @version <tt>$Revision$</tt>
+ */
+public final class Unicode
+{
+    /*----------------------------------------------------------------------*\
+                                 Constants
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * Copyright symbol.
+     */
+    public static char COPYRIGHT_SIGN = '\u00a9';
+
+    /**
+     * The "registered" symbol ("R" in a circle).
+     */
+    public static char REGISTERED_SIGN = '\u00ae';
+
+    /**
+     * "One-quarter" (1/4) symbol.
+     */
+    public static char ONE_QUARTER_FRACTION = '\u00bc';
+
+    /**
+     * "One-half" (1/2) symbol.
+     */
+    public static char ONE_HALF_FRACTION = '\u00bd';
+
+    /**
+     * "Three-quarters" (3/4) symbol.
+     */
+    public static char THREE_QUARTERS_FRACTION = '\u00be';
+
+    /**
+     * Classic mathematical multiplication ("times") symbol.
+     */
+    public static char MULTIPLICATION_SIGN = '\u00d7';
+
+    /**
+     * Classic mathematical division symbol.
+     */
+    public static char DIVISION_SIGN = '\u00f7';
+
+    /**
+     * Symbol for degrees
+     */
+    public static char DEGREE_SIGN = '\u00b0';
+
+    /**
+     * Non-breaking space.
+     */
+    public static char NBSP = '\u00a0';
+}
