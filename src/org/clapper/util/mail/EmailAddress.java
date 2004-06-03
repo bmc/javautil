ce247a1aa0d240d8719bbabc4151c5808b890a64
Brian Clapper <bmc@clapper.org>
2004-06-03 15:51:24 +0000

    Added license and copyright information.

--- src/org/clapper/util/mail/EmailAddress.java
+++ src/org/clapper/util/mail/EmailAddress.java
@@ -1,19 +1,35 @@
 /*---------------------------------------------------------------------------*\
   $Id$
+  ---------------------------------------------------------------------------
+  This software is released under a Berkeley-style license:
+
+  Copyright (c) 2004 Brian M. Clapper. All rights reserved.
+
+  Redistribution and use in source and binary forms are permitted provided
+  that: (1) source distributions retain this entire copyright notice and
+  comment; and (2) modifications made to the software are prominently
+  mentioned, and a copy of the original software (or a pointer to its
+  location) are included. The name of the author may not be used to endorse
+  or promote products derived from this software without specific prior
+  written permission.
+
+  THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+  Effectively, this means you can do what you want with the software except
+  remove this notice or take advantage of the author's name. If you modify
+  the software and redistribute your modified version, you must indicate that
+  your version is a modification of the original, and you must provide either
+  a pointer to or a copy of the original.
 \*---------------------------------------------------------------------------*/
 
 package org.clapper.util.mail;
 
 import java.util.Collection;
 
-/*
-import java.io.*;
-import java.net.*;
-import javax.mail.*;
-*/
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.AddressException;
-//import javax.activation.*;
 
 /**
  * This class encodes an RFC822-compliant email address is a simplified
@@ -22,7 +38,8 @@ import javax.mail.internet.AddressException;
  * @see EmailMessage
  *
  * @version <tt>$Revision$</tt>
- * @level developer
+ *
+ * @author Copyright &copy; 2004 Brian M. Clapper
  */
 public class EmailAddress implements Cloneable, Comparable
 {
