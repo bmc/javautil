f2728dbde9d28ff4edddcf111d3df521e8f05467
Brian Clapper <bmc@clapper.org>
2004-08-17 18:43:43 +0000

    Version 1.0

--- src/org/clapper/util/misc/Version.java
+++ src/org/clapper/util/misc/Version.java
@@ -46,7 +46,7 @@ public final class Version
                              Public Constants
     \*----------------------------------------------------------------------*/
 
-    public static final String API_VERSION = "0.9";
+    public static final String API_VERSION = "1.0";
 
     /**
      * The name of the resource bundle containing the build info.
