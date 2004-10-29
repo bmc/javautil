55f9fb5f3e5d30183d34909d02ccb338cc60445c
Brian Clapper <bmc@clapper.org>
2004-10-29 17:15:33 +0000

    New.

---
+++ src/org/clapper/util/logging/LogLevel.java
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------*\
+  $Id$
+  ---------------------------------------------------------------------------
+  This software is released under a Berkeley-style license:
+
+  Copyright (c) 2004 Brian M. Clapper. All rights reserved.
+
+  Redistribution and use in source and binary forms are permitted provided
+  that: (1) source distributions retain this entire copyright notice and
+  comment; and (2) modifications made to the software are prominently
+  mentioned, and a copy of the original software (or a pointer to its
+  location) are included. The name of the author may not be used to endorse
+  or promote products derived from this software without specific prior
+  written permission.
+
+  THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+  Effectively, this means you can do what you want with the software except
+  remove this notice or take advantage of the author's name. If you modify
+  the software and redistribute your modified version, you must indicate that
+  your version is a modification of the original, and you must provide either
+  a pointer to or a copy of the original.
+\*---------------------------------------------------------------------------*/
+
+package org.clapper.util.logging;
+
+import java.util.logging.Level;
+
+/**
+ * Encodes a legal logging levels for the {@link Logger} class.
+ */
+public class LogLevel
+{
+    /*----------------------------------------------------------------------*\
+                           Private Instance Data
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * The level.
+     */
+    private Level level;
+
+    /*----------------------------------------------------------------------*\
+                                Constructor
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * Construct a new <tt>LogLevel</tt> object.
+     *
+     * @param level the level
+     */
+    LogLevel (Level level)
+    {
+        this.level = level;
+    }
+
+    /*----------------------------------------------------------------------*\
+                          Package-visible Methods
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * Get this object's <tt>Level</tt>
+     *
+     * @return the associated <tt>Level</tt> object
+     */
+    Level getLevel()
+    {
+        return level;
+    }
+}
