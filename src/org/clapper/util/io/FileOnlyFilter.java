c0e04e72e669308caf3521d8b2e6baea46a520a7
Brian Clapper <bmc@clapper.org>
2006-05-11 15:09:11 +0000

    Added new org.clapper.util.io.FileOnlyFilter, a complement to the org.clapper.util.io.DirectoryFilter class. Like DirectoryFilter, FileOnlyFilter implements java.io.FileFilter and can be used to filter files.

---
+++ src/org/clapper/util/io/FileOnlyFilter.java
@@ -0,0 +1,62 @@
+/*---------------------------------------------------------------------------*\
+  $Id: DirectoryFilter.java 5582 2005-04-23 00:37:10Z bmc $
+  ---------------------------------------------------------------------------
+  This software is released under a Berkeley-style license:
+
+  Copyright (c) 2004-2005 Brian M. Clapper. All rights reserved.
+
+  Redistribution and use in source and binary forms are permitted provided
+  that: (1) source distributions retain this entire copyright notice and
+  comment; and (2) modifications made to the software are prominently
+  mentioned, and a copy of the original software (or a pointer to its
+  location) are included. The name of the author may not be used to endorse
+  or promote products derived from this software without specific prior
+  written permission.
+
+  THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+  Effectively, this means you can do what you want with the software except
+  remove this notice or take advantage of the author's name. If you modify
+  the software and redistribute your modified version, you must indicate that
+  your version is a modification of the original, and you must provide either
+  a pointer to or a copy of the original.
+\*---------------------------------------------------------------------------*/
+
+package org.clapper.util.io;
+
+import java.io.*;
+
+/**
+ * <tt>FileOnlyFilter</tt> implements a
+ * <tt>java.io.FileFilter</tt> that matches only directories.
+ *
+ * @version <tt>$Revision: 5582 $</tt>
+ *
+ * @author Copyright &copy; 2004 Brian M. Clapper
+ */
+public class FileOnlyFilter implements FileFilter
+{
+    /*----------------------------------------------------------------------*\
+                                Constructor
+    \*----------------------------------------------------------------------*/
+
+    public FileOnlyFilter()
+    {
+    }
+
+    /*----------------------------------------------------------------------*\
+                              Public Methods
+    \*----------------------------------------------------------------------*/
+
+    /**
+     * Determine whether the specified file is a directory or not.
+     *
+     * @return <tt>true</tt> if the file is a directory, <tt>false</tt> if not
+     */
+    public boolean accept (File f)
+    {
+        return f.isFile();
+    }
+}
