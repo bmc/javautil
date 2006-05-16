ecc51d9e10e6ae38a723c4a0745764047571557e
Brian Clapper <bmc@clapper.org>
2006-05-16 03:39:24 +0000

    Added version of add() that takes a File[]

--- src/org/clapper/util/classutil/ClassLoaderBuilder.java
+++ src/org/clapper/util/classutil/ClassLoaderBuilder.java
@@ -127,6 +127,29 @@ public class ClassLoaderBuilder
     }
 
     /**
+     * Add an array of jar files, zip files or directories to the list of
+     * places the not-yet-constructed class loader will search. If the
+     * directory or file does not exist, or isn't a jar file, zip file, or
+     * directory, this method just ignores it and returns <tt>false</tt>.
+     *
+     * @param files  the array
+     *
+     * @return the number of entries from the array that actually were added
+     */
+    public int add (File[] files)
+    {
+        int total = 0;
+
+        for (File f : files)
+        {
+            if (add (f))
+                total++;
+        }
+
+        return total;
+    }
+
+    /**
      * Add the contents of the classpath.
      */
     public void addClassPath()
