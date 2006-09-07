0a337f6a4f38901e59dd9385258883ae6eec41bb
Brian Clapper <bmc@clapper.org>
2006-09-07 02:26:05 +0000

    Added tester for WindowsCmdVariableSubstituter

--- junit/src/org/clapper/util/text/WindowsCmdVariableSubstituterTest.java
+++ junit/src/org/clapper/util/text/WindowsCmdVariableSubstituterTest.java
@@ -53,9 +53,9 @@ import junit.framework.*;
 /**
  *
  */
-public class UnixShellVariableSubstituterTest extends TestCase
+public class WindowsCmdVariableSubstituterTest extends TestCase
 {
-    public UnixShellVariableSubstituterTest(String testName)
+    public WindowsCmdVariableSubstituterTest(String testName)
     {
         super(testName);
     }
@@ -74,7 +74,7 @@ public class UnixShellVariableSubstituterTest extends TestCase
     public void testSubstitution()
         throws VariableSubstitutionException
     {
-        UnixShellVariableSubstituter sub = new UnixShellVariableSubstituter();
+        WindowsCmdVariableSubstituter sub = new WindowsCmdVariableSubstituter();
         Map<String,String> vars = new HashMap<String,String>();
         vars.put("a", "1");
         vars.put("b", "2");
@@ -84,41 +84,25 @@ public class UnixShellVariableSubstituterTest extends TestCase
         vars.put("longerVariableName", " ");
         MapVariableDereferencer deref = new MapVariableDereferencer(vars);
 
-        String s = sub.substitute("ab$a", deref);
+        String s = sub.substitute("ab%a%", deref);
         assertEquals("Substitution failure", "ab1", s);
 
-        s = sub.substitute("${a", deref);
-        assertEquals("Substitution failure", "${a", s);
+        s = sub.substitute("%a", deref);
+        assertEquals("Substitution failure", "%a", s);
 
-        s = sub.substitute("$a$b$c", deref);
+        s = sub.substitute("%a%%b%%c%", deref);
         assertEquals("Substitution failure", "12", s);
 
-        s = sub.substitute("$a$b$foo", deref);
+        s = sub.substitute("%a%%b%%foo%", deref);
         assertEquals("Substitution failure", "12barsky", s);
 
-        s = sub.substitute("$x", deref);
+        s = sub.substitute("%x%", deref);
         assertEquals("Substitution failure", "", s);
-
-        s = sub.substitute("$x?abc", deref);
-        assertEquals("Substitution failure", "?abc", s);
-
-        s = sub.substitute("${x?abc}", deref);
-        assertEquals("Substitution failure", "abc", s);
-
-        s = sub.substitute("${y?abc def ghi}", deref);
-        assertEquals("Substitution failure", "abc def ghi", s);
-
-        s = sub.substitute("${y??}", deref);
-        assertEquals("Substitution failure", "?", s);
-
-        s = sub.substitute("${longerVariableName?foo}", deref);
-        assertFalse("Substitution failure", "foo".equals(s));
-        assertEquals("Substitution failure", " ", s);
 
         sub.setAbortOnUndefinedVariable(true);
         try
         {
-            s = sub.substitute("${blah}", deref);
+            s = sub.substitute("%blah%", deref);
             fail("Expected UnknownVariableException");
         }
         catch (UndefinedVariableException ex)
@@ -130,7 +114,7 @@ public class UnixShellVariableSubstituterTest extends TestCase
         sub.setAbortOnSyntaxError(true);
         try
         {
-            s = sub.substitute("${foo", deref);
+            s = sub.substitute("%foo", deref);
             fail("Expected VariableSyntaxException");
         }
 
