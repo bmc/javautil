683c4d49549b61825c7bab10e961b994c94e73a6
Brian Clapper <bmc@clapper.org>
2006-09-12 14:01:21 +0000

    Renamed jsr223 subpackage to javax_script

--- src/org/clapper/util/scripting/ScriptFrameworkType.java
+++ src/org/clapper/util/scripting/ScriptFrameworkType.java
@@ -6,14 +6,19 @@ package org.clapper.util.scripting;
 
 /**
  * Defines the set of script frameworks supported by the
- * {@link ScriptExecutor} class.
+ * {@link ScriptExecutor} class. There are currently two legal values:
  *
- * @see ScriptExecutor#getScriptExecutor
+ * <ul>
+ *   <li> <tt>JAVAX_SCRIPT</tt>: use the <tt>javax.script</tt> (a.k.a.,
+ *        JSR 223) scripting infrastructure, available as of Java 6.
+ *   <li> <tt>BSF</tt>: Use the Apache Jakarta Bean Scripting Framework (BSF).
+ * </ul>
  *
  * @version <tt>$Revision$</tt>
+ * @see ScriptExecutor#getScriptExecutor
  */
 public enum ScriptFrameworkType
 {
-    JSR_223,
+    JAVAX_SCRIPT,
     BSF
 }
